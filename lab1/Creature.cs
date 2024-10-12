2df6184883adfbe7b2e3937db8fadf5efb810836
vladas9 <vladamza63@gmail.com>
2024-10-12 11:37:00 +0300

    Move creature class in Creature.cs
    move public values to private in InputData.cs and Creture.cs
    
    Creature.cs: add public properties to expose private fields, with getter-only access for most fields and a protected setter for univers, InputData.cs: add public properti to expose private field _input, with getter and setter

---
+++ lab1/Creature.cs
@@ -0,0 +1,29 @@
+public class Creature
+{
+    private readonly int _id;
+    private readonly bool? _isHumanoid;
+    private readonly string? _planet;
+    private readonly int? _age;
+    private readonly List<string>? _traits;
+    private string _univers;
+
+    public Creature(int id, bool? isHumanoid, string? planet, int? age, List<string>? traits)
+    {
+        _id = id;
+        _isHumanoid = isHumanoid;
+        _planet = planet;
+        _age = age;
+        _traits = traits;
+        _univers = "";
+    }
+    public int id => _id;
+    public bool? isHumanoid => _isHumanoid;
+    public string? planet => _planet;
+    public int? age => _age;
+    public List<string>? traits => _traits;
+    public string univers
+    {
+        get => _univers;
+        protected set => _univers = value;
+    }
+}
--- lab1/InputData.cs
+++ lab1/InputData.cs
@@ -1,14 +1,5 @@
 using System.Text.Json;
 
-public class Creature
-{
-    public int id { get; set; }
-    public bool? isHumanoid { get; set; }
-    public string? planet { get; set; }
-    public int? age { get; set; }
-    public List<string>? traits { get; set; }
-}
-
 public class InputData
 {
     public List<Creature>? input { get; set; }
