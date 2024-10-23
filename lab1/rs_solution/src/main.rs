c9dc95894d84db33fca4f2b5194824e322f57f32
vladas9 <vladamza63@gmail.com>
2024-10-23 21:29:26 +0300

    Finished tasks 2 and 4 in rust: Add file_reader module and text_data that holds all the info and methos for counting, also added a fmt disply for fetx data

--- lab1/rs_solution/src/main.rs
+++ lab1/rs_solution/src/main.rs
@@ -1,6 +1,10 @@
 mod display;
+mod file_reader;
+
+use std::{env, io, path::PathBuf};
 
 use display::Display;
+use file_reader::FileReader;
 
 fn task_1() {
     let display1: Display = Display::new(50, 100, 300.0, String::from("display1"));
@@ -12,6 +16,36 @@ fn task_1() {
     display3.compare_with_monitor(&display1);
 }
 
+fn task_2_and_4() {
+    let paths: Vec<PathBuf> = env::args().skip(1).map(PathBuf::from).collect();
+
+    let data_list = FileReader::read_txt_in_text_data(&paths);
+
+    for data in data_list {
+        println!("{}", &data);
+    }
+}
+
 fn main() {
-    task_1();
+    let mut choice: i32 = -1;
+    let read_number = || {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        input
+            .trim()
+            .parse::<i32>()
+            .expect("Please enter a valid number.")
+    };
+    while choice != 0 {
+        println!("Enter a number of task (1-4)");
+
+        choice = read_number();
+        match choice {
+            1 => task_1(),
+            2 | 4 => task_2_and_4(),
+            _ => println!("not a valid choice"),
+        }
+    }
 }
